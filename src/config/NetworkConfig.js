353601ab49c244800d340cbdc04a94e6ff382518
Yuanjianxin <1814888800@qq.com>
2018-05-17 19:20:16 +0800

    优化错误处理

--- src/config/NetworkConfig.js
+++ src/config/NetworkConfig.js
@@ -1,30 +1,38 @@
 /**
  * Created by yuanjianxin on 2018/3/16.
  */
-import Vue from 'vue'
-const baseUrl='http://192.168.1.34:4000/'
+import Vue from "vue";
+const baseUrl = 'http://127.0.0.1:8081/'
 let config={
 
   _beforeHttpFunc:function (para,headers={}) {
     //todo
+
+    if (para instanceof FormData)
+      return {para, headers};
+
     para=Object.assign({},{appId:'yue'},para)
 
-    //console.log('=====Vue',Vue);
     return {para,headers}
   },
 
   _afterHttpFunc:function (res) {
     //todo
-    if(res.code!=='OK'){
-      Vue.$messagebox.alert('网络异常！请稍后再试');
-      return null;
-    }
-    return res.data;
+
+    if (res && res.code == 200)
+      return res.data;
+
+    //这里处理下未捕获的异常
+    if (res.code == 'error')
+      return Vue.$messagebox.alert('网络异常！请稍后再试');
+
+    return Vue.$messagebox.alert(res.message);
   },
 
 
 
   GET_VERIFY_QUESTION:{ method:'get',url:'verifyQuestion'},
+  TEST_UPLOAD: {method: 'post', url: 'test'},
   SEND_SMS:{ method:'post',url:'sendSMS'},
   LOGIN:{ method:'post',url:'login'},
   GET_WS_INFO:{method:'post',url:'getWebSocketInfo'},
